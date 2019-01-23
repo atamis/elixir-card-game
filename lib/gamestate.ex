4287eb888ca0adc80fb94d4e141ccc31983c2b5d
Andrew Amis <atamiser@gmail.com>
2019-01-22 19:22:04 -0800

    Added new game code.

--- lib/gamestate.ex
+++ lib/gamestate.ex
@@ -4,18 +4,47 @@ defmodule GameState do
   @empty_state %{players: %{},
                  current: %{},
                  num_players: 0,
-                 play_area: nil,
+                 play_area: [],
                  deck: [],
                  discard: []}
+
+  @default_player %{hand: []}
 
   @deck [:deck]
   @players [:players]
   @discard [:discard]
   @topdeck @deck ++ [0]
-  @current_player @players ++ [{:dynamic, :curent}]
+  @topplay [:play_area, 0]
+  @current_player @players ++ [{:dynamic, :current}]
 
-  # Draws a card from the deck, shuffling the discard into the deck if empty.
-  # Returns {new_state, card}
+  def nth_player_lens(n), do: @players ++ [n]
+
+  @doc """
+  Executes a "game tick", returning `{:ok, state}` normally,
+  `{:ok, state, :ready}` if the state is ready for the next
+  play, and `{:ok, state, :requires_play}` if the player needs to
+  play a card (see tick/2)
+  """
+  def tick(%{play_area: []} = state) do
+    {:ok, state, :ready}
+  end
+
+  def tick(state) do
+    case Lens.view(state, @topplay) do
+      :requires_play -> {:ok, state, :requires_play}
+      card -> {:ok, card_dispatch(card).(state)}
+    end
+  end
+
+  def tick(%{play_area: [:requires_play | _]} = state, card) do
+    state = Lens.set(state, @topplay, card)
+    tick(state)
+  end
+
+  @doc """
+  Draws a card from the deck, shuffling the discard into the deck if empty.
+  Returns {new_state, card}
+  """
   def draw_card(state) do
     state = if state[:deck] == [] do
       discard = state |> Lens.view(@discard)
@@ -27,14 +56,47 @@ defmodule GameState do
       state
     end
 
-    {card, state} = Map.get_and_update(state, :deck, &(List.pop_at(&1, 0)))
+    {card, state} = state |> Lens.get_update(:deck, &pop/1)
 
     {state, card}
   end
 
+  @doc """
+  Find the player index of the next player.
+  """
   def next_player(%{num_players: num_players, current: current}) do
     rem(current + 1, num_players)
   end
+
+  def card_finished(state) do
+    {card, state} = Lens.get_update(state, [:play_area], &pop/1)
+
+    state |>
+    Lens.map([:discard], &(push(&1, card)))
+  end
+
+  @doc """
+  End the current player's turn.
+  """
+  def end_turn(state) do
+    state |> Lens.set([:current], next_player(state))
+  end
+
+  #require IEx; IEx.pry()
+
+  def card_dispatch(%{dispatch: :draw}) do
+    fn state ->
+      {state, card} = draw_card(state)
+
+
+      state
+      |> Lens.map(@current_player ++ [:hand], &(push(&1, card)))
+      |> card_finished()
+    end
+  end
+
+  defp push(lst, item), do: List.insert_at(lst, 0, item)
+  defp pop(lst), do: List.pop_at(lst, 0)
 
 end
 
--- test/gamestate_test.exs
+++ test/gamestate_test.exs
@@ -49,6 +49,40 @@ defmodule GameStateTest do
     # NB: this doesn't get the attribute, so it will break if that changes.
     assert :zero == state |> Lens.view([:players, {:dynamic, :current}])
 
+    assert :one == state |> Lens.view(GameState.nth_player_lens(1))
+  end
+
+  test "simple end turn" do
+    state1 = %{current: 0, num_players: 2, players: %{0 => :zero, 1 => :one}}
+
+    state2 = GameState.end_turn(state1)
+
+    assert %{state1 | current: 1} == state2
+
+    assert %{state2 | current: 0} == GameState.end_turn(state2)
+  end
+
+  test "card finished" do
+    state = %{play_area: [%{dispatch: :card1, data: 4}], discard: []}
+
+    assert %{play_area: [], discard: [%{dispatch: :card1, data: 4}]} == GameState.card_finished(state)
+  end
+
+  test "draw card card and game tick" do
+    state = %{current: 0,
+              players: %{0 => %{hand: []}},
+              deck: [:card1, :card2],
+              play_area: [%{dispatch: :draw}],
+              discard: []
+             }
+
+    {:ok, state2} = GameState.tick(state)
+
+    assert %{state | players: %{0 => %{hand: [:card1]}},
+             deck: [:card2],
+             play_area: [],
+             discard: [%{dispatch: :draw}]
+    } == state2
   end
 
 end
