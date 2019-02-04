6805f3a455d7b77b10934ebb8e5f9be65f3c2eef
Andrew Amis <atamiser@gmail.com>
2019-02-03 16:40:32 -0800

    Added a macro to make progressive map building easier.
    You'd normally have to deal with full if statements in order to conditionally
    update a map, or leave it unchanged, but with this macro, you can use a short
    macro call with block syntax.

--- lib/util.ex
+++ lib/util.ex
@@ -23,4 +23,25 @@ defmodule Util do
       end
     end
   end
+
+  @doc """
+  Macro to make conditionally updating a map easier. If conditional is true, it merges
+  the result of the body into the original. Otherwise, it returns the original, leaving
+  the body unevaluated.
+  """
+  defmacro cond_put(conditional, orig, body) do
+    quote generated: true, location: :keep do
+      original = unquote(orig)
+      if unquote(conditional) do
+        new_map = unquote(body)
+        case new_map do
+          [do: map] -> Map.merge(original, map)
+          # {key, value} -> Map.put(original, key, value)
+          map -> Map.merge(original, map)
+        end
+      else
+        original
+      end
+    end
+  end
 end
